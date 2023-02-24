7bf216b8690c129af271a53a9055ad14a471500d
Krzysztof Glimos <kglimos@gmail.com>
2023-02-24 09:52:47 +0100

    all shell scripts

---
+++ relational-databases/Bash Scripts: 5 Programs/fortune.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Program to tell a persons fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+
+GET_FORTUNE() {
+  if [[ ! $1 ]]
+  then
+    echo -e "Ask a yes or no question:\n"
+  else
+    echo -e "Try again. Make sure it ends with a question mark:\n"
+  fi
+  read QUESTION
+}
+
+GET_FORTUNE
+
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE again
+done
+
+N=$(( RANDOM % 6 ))
+echo ${RESPONSES[$N]}
